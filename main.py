c6d7e1635aca46e557128d4d5d21ea1ca834b369
Leonardo Henrique <68018921+le0henr1que@users.noreply.github.com>
2021-03-24 14:19:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome('C:/Path/to/chromedriver')
 driver.get("https://www.google.com.br")
+
 driver.find_element_by_name("q").send_keys("Rua Olivio Basilio Marçal - 420" + Keys.RETURN)
-driver.find_element_by_class_name("hide-focus-ring").send_keys( Keys.RETURN)
-driver.find_element("Rota").send_keys( Keys.RETURN)
+driver.find_element_by_class_name("yYlJEf ").send_keys( Keys.RETURN) 
+driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[2]/button').send_keys( Keys.RETURN)
+
+driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[3]/button').send_keys( Keys.RETURN)
+
+driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[4]/button').send_keys( Keys.RETURN)
+
+driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[5]/button').send_keys( Keys.RETURN)
+
+driver.find_element_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[6]/button').send_keys( Keys.RETURN)
+
+
+
+#driver.find_element("Rota").send_keys( Keys.RETURN)
+
+#driver.quit()
